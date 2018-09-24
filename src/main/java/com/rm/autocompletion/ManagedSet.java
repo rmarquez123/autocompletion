c57e62e2823166aba124fd0041b0078c54a2f8ce
rmarquez123 <rmarquez123@gmail.com>
2018-09-24 15:25:06 -0700

    small refactoring.

--- src/main/java/com/rm/autocompletion/ManagedSet.java
+++ src/main/java/com/rm/autocompletion/ManagedSet.java
@@ -30,12 +30,35 @@ public class ManagedSet {
   }
 
   /**
+   * Sets the completion items to a list property. A change event will be
+   * triggered.
    *
    * @param items
    */
   public void setCompletionItems(List<CompletionItem> items) {
     this.textItems.setValue(FXCollections.observableArrayList(items));
+  }
 
+  /**
+   * Gets candidate completion items. Applies grouping when there are groupings
+   * (for example a.1, a.2 are a group of a).
+   *
+   * @param cursorWord Used to get the preliminary set of completion items.
+   * @return List of Completion Items obtained from the managed list of
+   * completion items based on the cursorWord.
+   */
+  public CandidateCompletionItems getCandidateCompletionItems(CursorWord cursorWord) {
+    CandidateCompletionItems result;
+    Set<CompletionItem> preliminary = this.getPreliminaryCandidatesAsSet(cursorWord);
+    Set<CompletionItem> items = this.applyGrouping(preliminary);
+    List<CompletionItem> itemsList;
+    if ((items.size() == 1 && this.getJoin(items).isEmpty())) {
+      itemsList = Arrays.asList(new CompletionItem(cursorWord.getText(), cursorWord.getText(), null));
+    } else {
+      itemsList = items.stream().sorted().collect(Collectors.toList());
+    }
+    result = new CandidateCompletionItems(itemsList);
+    return result;
   }
 
   /**
@@ -43,8 +66,7 @@ public class ManagedSet {
    * @param cursorWord
    * @return
    */
-  public CandidateCompletionItems getCandidateCompletionItems(CursorWord cursorWord) {
-    CandidateCompletionItems result;
+  private Set<CompletionItem> getPreliminaryCandidatesAsSet(CursorWord cursorWord) {
     Set<CompletionItem> preliminary = this.textItems.getValue().stream().filter((completionItems) -> {
       return this.filter(cursorWord, completionItems);
     }).map((ci) -> {
@@ -60,24 +82,25 @@ public class ManagedSet {
       CompletionItem completionItem = new CompletionItem(key, i.getInsertionText(), i.getObject());
       return completionItem;
     }).collect(Collectors.toSet());
+    return preliminary;
+  }
 
+  /**
+   *
+   * @param preliminary
+   * @return
+   */
+  private Set<CompletionItem> applyGrouping(Set<CompletionItem> preliminary) {
     Set<CompletionItem> items = new HashSet<>();
     preliminary.stream().forEach((item) -> {
       if (this.textItems.getValue().contains(item)) {
-        items.add(item); 
+        items.add(item);
       } else {
-        CompletionItem groupCompletionItem = new CompletionItem(item.getKey(), null, null); 
-        items.add(groupCompletionItem); 
+        CompletionItem groupCompletionItem = new CompletionItem(item.getKey(), null, null);
+        items.add(groupCompletionItem);
       }
     });
-    List<CompletionItem> itemsList;
-    if ((items.size() == 1 && this.getJoin(items).isEmpty())) {
-      itemsList = Arrays.asList(new CompletionItem(cursorWord.getText(), cursorWord.getText(), null));
-    } else {
-      itemsList = items.stream().sorted().collect(Collectors.toList());
-    }
-    result = new CandidateCompletionItems(itemsList);
-    return result;
+    return items;
   }
 
   /**
